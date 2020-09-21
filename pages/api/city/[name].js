d4b802a2dacd030e2a5c55c353c5c33e942d1e72
hexamesoft <71631802+hexamesoft@users.noreply.github.com>
2020-09-21 15:45:50 +0700

    Update [name].js

--- pages/api/city/[name].js
+++ pages/api/city/[name].js
@@ -1,14 +1,15 @@
-import firebase from '../../../lib/firebase';
+//import firebase from '../../../lib/firebase';
 
 export default (req, res) => {
-  firebase
-    .collection('cities')
-    .doc(req.query.name)
-    .get()
-    .then((doc) => {
-      res.json(doc.data());
-    })
-    .catch((error) => {
-      res.json({ error });
-    });
+  // firebase
+  //   .collection('cities')
+  //   .doc(req.query.name)
+  //   .get()
+  //   .then((doc) => {
+  //     res.json(doc.data());
+  //   })
+  //   .catch((error) => {
+  //     res.json({ error });
+  //   });
+  res.json({query: JSON.stringify(req.query), population: 12312321})
 };
